3645d2782d75a917dcb0617c00a855b861153766
miogy <jjangrl87@gmail.com>
2023-02-22 23:56:36 +0900

    Add: tab 만들기

--- src/page/About.js
+++ src/page/About.js
@@ -23,13 +23,15 @@ function About() {
   return (
     <>
       <button type="button" onClick={() => setTab(0)}>
-        Menu1
+        Step1. tab에 사용할 menu의 index별 컴포넌트를 담아준다.
       </button>
       <button type="button" onClick={() => setTab(1)}>
-        Menu2
+        Step2. tab을 state로 만들어 버튼태그를 이용하여 setState에 해당 index를
+        전달한다.
       </button>
       <button type="button" onClick={() => setTab(2)}>
-        Menu3
+        Step3. 컴포넌트들을 만든후 해당 컴포넌트가 들어갈 콘텐츠 자리에
+        &#123;tabComponents[tab]&#125;으로 사용한다.
       </button>
       {tabComponents[tab]}
     </>

e7e00068ab4b3499ef4c4bf607ffbf4aa3d1b09e
miogy <jjangrl87@gmail.com>
2023-02-22 23:57:55 +0900

    Add: tab Step수정

--- src/page/About.js
+++ src/page/About.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 
 export function Menu1() {
-  return <>Menu 1번 입니다.</>;
+  return <> Step1. tab에 사용할 menu의 index별 컴포넌트를 담아준다.</>;
 }
 
 export function Menu2() {
-  return <>Menu 2번 입니다.</>;
+  return (
+    <>
+      Step2. tab을 state로 만들어 버튼태그를 이용하여 setState에 해당 index를
+      전달한다.
+    </>
+  );
 }
 
 export function Menu3() {
-  return <>Menu 3번 입니다.</>;
+  return (
+    <>
+      Step3. 컴포넌트들을 만든후 해당 컴포넌트가 들어갈 콘텐츠 자리에
+      &#123;tabComponents[tab]&#125;으로 사용한다.
+    </>
+  );
 }
 
 function About() {
@@ -23,15 +33,13 @@ function About() {
   return (
     <>
       <button type="button" onClick={() => setTab(0)}>
-        Step1. tab에 사용할 menu의 index별 컴포넌트를 담아준다.
+        Step1
       </button>
       <button type="button" onClick={() => setTab(1)}>
-        Step2. tab을 state로 만들어 버튼태그를 이용하여 setState에 해당 index를
-        전달한다.
+        Step2
       </button>
       <button type="button" onClick={() => setTab(2)}>
-        Step3. 컴포넌트들을 만든후 해당 컴포넌트가 들어갈 콘텐츠 자리에
-        &#123;tabComponents[tab]&#125;으로 사용한다.
+        Step3
       </button>
       {tabComponents[tab]}
     </>

359f444578cd9e4a41f3cfbb3b1eae56ef613aa5
miogy <jjangrl87@gmail.com>
2023-02-22 23:49:44 +0900

    Add: tab

--- src/page/About.js
+++ src/page/About.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+export function Menu1() {
+  return <>Menu 1번 입니다.</>;
+}
+
+export function Menu2() {
+  return <>Menu 2번 입니다.</>;
+}
+
+export function Menu3() {
+  return <>Menu 3번 입니다.</>;
+}
 
 function About() {
-  return <>About</>;
+  const [tab, setTab] = useState(0);
+
+  const tabComponents = {
+    0: <Menu1 />,
+    1: <Menu2 />,
+    2: <Menu3 />,
+  };
+  return (
+    <>
+      <button type="button" onClick={() => setTab(0)}>
+        Menu1
+      </button>
+      <button type="button" onClick={() => setTab(1)}>
+        Menu2
+      </button>
+      <button type="button" onClick={() => setTab(2)}>
+        Menu3
+      </button>
+      {tabComponents[tab]}
+    </>
+  );
 }
 
 export default About;

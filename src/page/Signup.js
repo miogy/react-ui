a23b86c753999bed78c43fbf95d4e8a8dae1b5e6
miogy <jjangrl87@gmail.com>
2023-02-24 23:29:14 +0900

    Add: signup formData onChange

--- src/page/Signup.js
+++ src/page/Signup.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initFormData = {
+  id: "",
+  password: "",
+  passwordCfm: "",
+};
 
 function Signup() {
+  const [formData, setFormData] = useState(initFormData);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <>
       <form>
         <label>아이디</label>
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <label>비밀번호</label>
-        <input type="password" />
+        <input type="password" onChange={handleChange} />
         <label>비밀번호 확인</label>
-        <input type="password" />
+        <input type="password" onChange={handleChange} />
       </form>
     </>
   );

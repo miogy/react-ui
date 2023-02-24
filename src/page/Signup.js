78749ee98d5358c1ff818c6ad4a2c7ae08c67e6e
miogy <jjangrl87@gmail.com>
2023-02-24 23:26:27 +0900

    Add: signup ui

--- src/page/Signup.js
+++ src/page/Signup.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Signup() {
-  return <></>;
+  return (
+    <>
+      <form>
+        <label>아이디</label>
+        <input type="text" />
+        <label>비밀번호</label>
+        <input type="password" />
+        <label>비밀번호 확인</label>
+        <input type="password" />
+      </form>
+    </>
+  );
 }
 
 export default Signup;

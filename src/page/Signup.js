61301587c71fd8dccfc315ac5505156483d842e8
miogy <jjangrl87@gmail.com>
2023-02-24 23:37:00 +0900

    Add: input radio

--- src/page/Signup.js
+++ src/page/Signup.js
@@ -12,8 +12,40 @@ function Signup() {
   const handleChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
+
+  const handleChangeRadio = (name) => {
+    setFormData((prev) => ({ ...prev, [name.target.name]: name.target.value }));
+  };
   return (
     <>
+      <div className="checkbox_radio">
+        <input
+          onChange={handleChangeRadio}
+          type="radio"
+          name="option"
+          value="1"
+          checked={formData.startup === "1"}
+        />
+        <label htmlFor="option"> option 1 </label>
+        <input
+          type="radio"
+          name="option"
+          value="2"
+          checked={formData.startup === "2"}
+          onChange={handleChangeRadio}
+          defaultChecked
+        />
+        <label htmlFor="option"> option 2 </label>
+        <input
+          type="radio"
+          name="option"
+          value="3"
+          checked={formData.startup === "3"}
+          onChange={handleChangeRadio}
+        />
+        <label htmlFor="option"> option 3 </label>
+      </div>
+
       <form>
         {/* input type=file을 보내는 폼에 enctype="multipart/form-data" 속성을 넣어 줘야함 */}
         <label htmlFor="id">아이디</label>

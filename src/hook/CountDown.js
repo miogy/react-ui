621b07de2806d3318513a35df66cb854bc27d3db
miogy <jjangrl87@gmail.com>
2023-02-27 22:31:39 +0900

    Add: count down => home

--- src/hook/CountDown.js
+++ src/hook/CountDown.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CountDown() {
-  return <></>;
+  const [seconds, setSeconds] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer =
+      seconds > 0 && setInterval(() => setSeconds(seconds - 1), 1000);
+    return () => clearInterval(timer);
+  }, [seconds]);
+
+  useEffect(() => {
+    if (Response.error) {
+      setTimeout(() => {
+        navigate("/");
+      }, 5000);
+    }
+  }, []);
+  return (
+    <>
+      <p>
+        {seconds}
+        <span>초 후</span>
+        <button
+          type="button"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          HOME
+        </button>
+        <span>으로 이동됩니다.</span>
+      </p>
+    </>
+  );
 }
 
 export default CountDown;

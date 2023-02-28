9f415571af7b85b1615b1fe71e145861efd349b8
miogy <jjangrl87@gmail.com>
2023-02-28 23:41:48 +0900

    Add: usePreventLeave hook

--- src/hook/usePreventLeave.js
+++ src/hook/usePreventLeave.js
@@ -1,7 +1,15 @@
-import React from "react";
-
-function UsePreventLeave() {
-  return <></>;
-}
-
-export default UsePreventLeave;
+const usePreventLeave = () => {
+  // 사용자의 페이지 이탈을 막음.
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  // 페이지 이탈 방지 활성화
+  const enablePrevent = () => {
+    window.addEventListener("beforeunload", listener);
+  };
+  // 이탈 방지 불활성화
+  const disablePrevent = () => {
+    window.removeEventListener("beforeunload", listener);
+  };
+};

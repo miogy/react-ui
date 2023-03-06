a1e2bd6ebdbae39ac38ac95798f6e044d9c8fd70
miogy <jjangrl87@gmail.com>
2023-03-06 23:24:48 +0900

    Add: 여러개의 토글 개별관리

--- src/page/Toggle.js
+++ src/page/Toggle.js
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
 
 function Toggle() {
+  // 1개의 토글 사용시 prev => !prev
   const [toggle, setToggle] = useState(false);
+  // 여러개의 토글 사용시
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (id) => {
+    if (activeButton === id) {
+      setActiveButton(null);
+    } else {
+      setActiveButton(id);
+    }
+  };
 
   return (
-    <div style={{ width: "200px", height: "300px", margin: "0 auto" }}>
-      <button type="button" onClick={() => setToggle((prev) => !prev)}>
-        {toggle ? <>open</> : <>close</>}
-      </button>
-      {toggle && <div>toggle open</div>}
+    <div style={{ width: "500px", height: "800px", margin: "0 auto" }}>
+      <div style={{ width: "100%", height: "100px", margin: "0 auto" }}>
+        <button type="button" onClick={() => setToggle((prev) => !prev)}>
+          {toggle ? <>close</> : <>open</>}
+        </button>
+        {toggle && <div>toggle open</div>}
+      </div>
+      <div>
+        <button type="button" onClick={() => handleClick(0)}>
+          {activeButton === 0 ? <>close</> : <>open</>}
+        </button>
+        {activeButton === 0 && <dlv>1번 toggle open</dlv>}
+      </div>
+      <div>
+        <button type="button" onClick={() => handleClick(1)}>
+          {activeButton === 1 ? <>close</> : <>open</>}
+        </button>
+        {activeButton === 1 && <dlv>2번 toggle open</dlv>}
+      </div>
+      <div>
+        <button type="button" onClick={() => handleClick(2)}>
+          {activeButton === 2 ? <>close</> : <>open</>}
+        </button>
+        {activeButton === 2 && <dlv>3번 toggle open</dlv>}
+      </div>
     </div>
   );
 }

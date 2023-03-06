b6c18381641754bddab010de3bf0f03b762f5331
miogy <jjangrl87@gmail.com>
2023-03-06 23:17:18 +0900

    Add: 개별 토글

--- src/page/Toggle.js
+++ src/page/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <></>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div style={{ width: "200px", height: "300px", margin: "0 auto" }}>
+      <button type="button" onClick={() => setToggle((prev) => !prev)}>
+        {toggle ? <>open</> : <>close</>}
+      </button>
+      {toggle && <div>toggle open</div>}
+    </div>
+  );
 }
 
 export default Toggle;
